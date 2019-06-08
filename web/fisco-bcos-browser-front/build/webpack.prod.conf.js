c3c3aa8d3b59a6bd6671f7ac5e52424ba6a9baa4
韩鹏飞 <413024870@qq.com>
2019-06-08 15:03:40 +0800

    update webpack

--- web/fisco-bcos-browser-front/build/webpack.prod.conf.js
+++ web/fisco-bcos-browser-front/build/webpack.prod.conf.js
@@ -20,6 +20,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     })
   },
   devtool: config.build.productionSourceMap ? '#source-map' : false,
+    node: { fs: "empty" },
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
